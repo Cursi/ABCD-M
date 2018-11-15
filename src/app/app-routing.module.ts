4e5fb13e4e97af71565e9df9adb10f95e21828d8
Cursi <razvan.diib@yahoo.com>
2018-11-15 03:47:51 +0200

    Playing around with mat-colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { TestComponent } from './test/test.component';
+import { ResultsComponent } from './results/results.component';
+
+const routes: Routes =
+[
+  { path: '', component: AboutComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'results', component: ResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,9 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatRadioModule} from '@angular/material';
 
-import {NgModule} from '@angular/core'; 
+import {NgModule} from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatRadioModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatRadioModule]
 })
 export class MaterialModule { }
